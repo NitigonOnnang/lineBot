17d4a865f2ad6b05dc2c89899e97672496b8f544
NitigonOnnang <nitigon@gmail.com>
2019-09-14 11:24:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,8 +2,9 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'ouhqskdRP/sUP8uwpjAadPDJz6rj1Y3IR0/ZznmHBgsPmYq6Q+hzdEJ4OXgyw/8NaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c0XIOEKRfUEQAsHVi4PbQU4HEk9GOq/cmdR3iRkQE9e5gdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
+$ACCESS_TOKEN = 'YGKuLyWmQ1sGRiUL3x9wIXaICjtftwTrZEjq8WHBH3XVS19DjGTSjbjv6CYehAHluWpRuwr/7xpyPc/gCyAPDKoGAG+YqSA7XtwDATXGyF/7lAJI0HHHKZyX8uz5zDLQ25yuKF+VAlQxOZVL30DRTlGUYhWQfeY8sLGRXgo3xvw=
+'; 
+$channelSecret = '2522462dc4ed01eb2dd44f5f78617d3d';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
